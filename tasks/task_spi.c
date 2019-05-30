8c5149d197a081560b3620932cf8e4ed075566f3
Otso Jousimaa <otso@ojousima.net>
2019-05-30 13:51:50 +0300

    use updated pin definitions

--- tasks/task_spi.c
+++ tasks/task_spi.c
@@ -6,6 +6,7 @@
  **/
 #include "ruuvi_boards.h"
 #include "ruuvi_driver_error.h"
+#include "ruuvi_interface_gpio.h"
 #include "ruuvi_interface_log.h"
 #include "ruuvi_interface_spi.h"
 #include "task_spi.h"
@@ -13,15 +14,15 @@
 ruuvi_driver_status_t task_spi_init(void)
 {
   ruuvi_interface_spi_init_config_t config;
-  uint8_t ss_pins[] = RUUVI_BOARD_SPI_SS_LIST;
-  config.mosi = RUUVI_BOARD_SPI_MOSI_PIN;
-  config.miso = RUUVI_BOARD_SPI_MISO_PIN;
-  config.sclk = RUUVI_BOARD_SPI_SCLK_PIN;
+  ruuvi_interface_gpio_id_t ss_pins[] = RUUVI_BOARD_SPI_SS_LIST;
+  config.mosi.pin = RUUVI_BOARD_SPI_MOSI_PIN;
+  config.miso.pin = RUUVI_BOARD_SPI_MISO_PIN;
+  config.sclk.pin = RUUVI_BOARD_SPI_SCLK_PIN;
   config.ss_pins = ss_pins;
-  config.ss_pins_number = sizeof(ss_pins);
+  config.ss_pins_number = sizeof(ss_pins)/sizeof(ruuvi_interface_gpio_id_t);
+
   // Assume mode 0 always.
   config.mode = RUUVI_INTERFACE_SPI_MODE_0;
-
   switch(RUUVI_BOARD_SPI_FREQ)
   {
     case RUUVI_BOARD_SPI_FREQUENCY_1M:
@@ -42,9 +43,7 @@ ruuvi_driver_status_t task_spi_init(void)
 
     default:
       config.frequency = RUUVI_INTERFACE_SPI_FREQUENCY_1M;
-      ruuvi_interface_log(RUUVI_INTERFACE_LOG_WARNING,
-                          "Unknown SPI frequency, defaulting to 1M\r\n");
+      ruuvi_interface_log(RUUVI_INTERFACE_LOG_WARNING, "Unknown SPI frequency, defaulting to 1M\r\n");
   }
-
   return ruuvi_interface_spi_init(&config);
 }
